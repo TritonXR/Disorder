b85bb7fda64cc834c1cf3c9d0de737df46e77d6f
c2du <c2du@UCSD.EDU>
2017-02-19 16:05:50 -0800

    Cutting and start of timing
    Cutting has bugs and timing is in progress

---
+++ Assets/Data_tracker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+using System.IO;
+
+public class Data_tracker: MonoBehaviour {
+
+    VRNode head = VRNode.Head;
+    VRNode lefthand = VRNode.LeftHand;
+    VRNode righthand = VRNode.RightHand;
+    private StreamWrier file;
+
+	void Start()
+    {
+        file = new StreamWriter("data.txt");
+        file.WriteLine("Time| Head x y z rotx roty rotz| Left Hand x y z rotx roty rotz | Right Hand x y z rotx roty rotz");
+
+        StartCoroutine(RecordData());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator RecordData()
+    {
+        while (true)
+        {
+            file.WriteLine(Time.time + " | " + head.transform.position + " " + head.transform.rotation.eulerAngles + " | " +
+            leftHand.transform.position + " " + leftHand.transform.rotation.eulerAngles + " | " +
+            rightHand.transform.position + " " + rightHand.transform.rotation.eulerAngles);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
+
---
+++ Assets/Western_weapons/Bowie_Knife/Prefab/fix_object.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fix_object : MonoBehaviour {
+
+    public GameObject left_cube;
+    public GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnJointBreak(float breakForce)
+    {
+        left_cube.GetComponent<Rigidbody>().isKinematic = false;
+
+        left_cube.GetComponent<FixedJoint>().breakForce = 250;
+
+        
+        if(left_cube.transform.name == "c8")
+        {
+            Debug.Log(left_cube.transform.name);
+            StartCoroutine(delete_parent());
+        }
+    }
+
+    IEnumerator delete_parent()
+    {
+        Debug.Log("We are running delete_parent");
+        yield return new WaitForSeconds(3f);
+        parent.SetActive(false);
+    }
+}
