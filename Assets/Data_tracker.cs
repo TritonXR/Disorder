f285f93224555fdacdd41e5af7ae33bdb36435e3
c2du <c2du@UCSD.EDU>
2017-02-26 12:40:55 -0800

    Updated data tracker
    Changed VRNode back to GameObject

--- Assets/Data_tracker.cs
+++ Assets/Data_tracker.cs
@@ -1,39 +1,57 @@
 ﻿using UnityEngine;
-using UnityEngine.VR;
+using UnityEngine.UI;
 using System.Collections;
+using System;
 using System.IO;
 
-public class Data_tracker: MonoBehaviour {
+public class Timer : MonoBehaviour
+{
+    //File to record the data
+    public StreamWriter file;
 
-    VRNode head = VRNode.Head;
-    VRNode lefthand = VRNode.LeftHand;
-    VRNode righthand = VRNode.RightHand;
-    private StreamWriter file;
+    //Movement of head and both hands
+    public GameObject head;
+    public GameObject leftHand;
+    public GameObject rightHand;
 
-	void Start()
+    //Timer
+    private float startTime;
+    private float totalTime;
+    private bool finished = false;
+
+    void Start()
     {
+        //TODO need to add the user name to the file
         file = new StreamWriter("data.txt");
-        file.WriteLine("Time| Head x y z rotx roty rotz| Left Hand x y z rotx roty rotz | Right Hand x y z rotx roty rotz");
-
+        file.WriteLine("Time | Head x y z rotx roty rotz | Left Hand x y z rotx roty rotz | Right Hand x y z rotx roty rotz");
+        startTime = Time.time;
         StartCoroutine(RecordData());
-
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (finished)
+            return;
 
+        totalTime = Time.time - startTime;
     }
 
     IEnumerator RecordData()
     {
         while (true)
         {
-           // file.WriteLine(Time.time + " | " + head.transform.position + " " + head.transform.rotation.eulerAngles + " | " +
-           // leftHand.transform.position + " " + leftHand.transform.rotation.eulerAngles + " | " +
-           // rightHand.transform.position + " " + rightHand.transform.rotation.eulerAngles);
-           //yield return new WaitForSeconds(.1f);
+            file.WriteLine(head.transform.position + " " + head.transform.rotation.eulerAngles + "|" +
+                leftHand.transform.position + " " + leftHand.transform.rotation.eulerAngles + "|" +
+                rightHand.transform.position + " " + rightHand.transform.rotation.eulerAngles);
+            yield return new WaitForSeconds(.1f);
         }
     }
-}
 
+    public void Finish()
+    {
+        file.WriteLine(totalTime.ToString("f2") + head.transform.position + " " + head.transform.rotation.eulerAngles + "|" +
+                leftHand.transform.position + " " + leftHand.transform.rotation.eulerAngles + "|" +
+                rightHand.transform.position + " " + rightHand.transform.rotation.eulerAngles);
+        finished = true;
+    }
+}
