e4bc6e2cbf4486f19396ce50424b5356a30572ac
c2du <c2du@ucsd.edu>
2017-03-05 18:18:52 -0800

    Able to somewhat cut apple on cutting board

---
+++ Assets/cut_fruit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cut_fruit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.collider.CompareTag("Apple"))
+        {
+            other.collider.gameObject.transform.parent = null;
+            other.collider.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Knife"))
+        {
+            Debug.Log("Trigger Exit");
+            this.gameObject.transform.parent = null;
+            this.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+}
