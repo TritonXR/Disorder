ea74191aa9f9685b37f2a7e0fd932a911be19752
Connie Du <c2du@ucsd.edu>
2017-03-13 19:25:19 -0700

    added new lines to data tracker

--- Assets/Data_tracker.cs
+++ Assets/Data_tracker.cs
@@ -29,7 +29,7 @@ public class Data_tracker : MonoBehaviour
         //TODO need to add the user name to the file
 		filename = "data_" + System.DateTime.Now.ToString("MM-dd-yy_hh-mm-ss") + ".txt";
         file = new StreamWriter(filename);
-        file.WriteLine("Time\tHead\tx\ty\t\tz\trotx\troty\trotz\tLeft Hand\tx\ty\tz\trotx\troty\trotz\tRight Hand\tx\ty\tz\trotx\troty\trotz");
+        file.WriteLine("Time\tHead\tx\ty\t\tz\trotx\troty\trotz\tLeft Hand\tx\ty\tz\trotx\troty\trotz\tRight Hand\tx\ty\tz\trotx\troty\trotz\n");
         startTime = Time.time;
         StartCoroutine(RecordData());
     }
@@ -52,12 +52,12 @@ public class Data_tracker : MonoBehaviour
             //Debug.Log(totalTime.ToString());
             //if (started)
             //{
-			file.WriteLine(totalTime.ToString() + "\t" + head.transform.position.x + "\t" + head.transform.position.y + "\t" + head.transform.position.z + "\t"
+			file.WriteLine(totalTime.ToString() + "\tx\t" + head.transform.position.x + "\t" + head.transform.position.y + "\t" + head.transform.position.z + "\tx\t"
 				+ head.transform.rotation.eulerAngles.x + "\t" + head.transform.rotation.eulerAngles.y + "\t" + head.transform.rotation.eulerAngles.z + "\t" 
-				+ leftHand.transform.position.x + "\t" + leftHand.transform.position.y + "\t" + leftHand.transform.position.z + "\t"
+				+ leftHand.transform.position.x + "\t" + leftHand.transform.position.y + "\t" + leftHand.transform.position.z + "\tx\t"
 				+ leftHand.transform.rotation.eulerAngles.x + "\t" + leftHand.transform.rotation.eulerAngles.y + "\t" + leftHand.transform.rotation.eulerAngles.z + "\t"
-				+ rightHand.transform.position.x + "\t" + rightHand.transform.position.y + "\t" + rightHand.transform.position.z + "\t"
-				+ rightHand.transform.rotation.eulerAngles.x + "\t" + rightHand.transform.rotation.eulerAngles.y + "\t" + rightHand.transform.rotation.eulerAngles.z + "\t");
+				+ rightHand.transform.position.x + "\t" + rightHand.transform.position.y + "\t" + rightHand.transform.position.z + "\tx\t"
+				+ rightHand.transform.rotation.eulerAngles.x + "\t" + rightHand.transform.rotation.eulerAngles.y + "\t" + rightHand.transform.rotation.eulerAngles.z + "\t\n");
             //}
             yield return new WaitForSeconds(.1f);
         }
@@ -65,12 +65,12 @@ public class Data_tracker : MonoBehaviour
 
     public void Finish()
     {
-		file.WriteLine(totalTime.ToString("f2") + "\t" + head.transform.position.x + "\t" + head.transform.position.y + "\t" + head.transform.position.z + "\t"
+		file.WriteLine(totalTime.ToString("f2") + "\tx\t" + head.transform.position.x + "\t" + head.transform.position.y + "\t" + head.transform.position.z + "\tx\t"
 			+ head.transform.rotation.eulerAngles.x + "\t" + head.transform.rotation.eulerAngles.y + "\t" + head.transform.rotation.eulerAngles.z + "\t" 
-			+ leftHand.transform.position.x + "\t" + leftHand.transform.position.y + "\t" + leftHand.transform.position.z + "\t"
+			+ leftHand.transform.position.x + "\t" + leftHand.transform.position.y + "\t" + leftHand.transform.position.z + "\tx\t"
 			+ leftHand.transform.rotation.eulerAngles.x + "\t" + leftHand.transform.rotation.eulerAngles.y + "\t" + leftHand.transform.rotation.eulerAngles.z + "\t"
-			+ rightHand.transform.position.x + "\t" + rightHand.transform.position.y + "\t" + rightHand.transform.position.z + "\t"
-			+ rightHand.transform.rotation.eulerAngles.x + "\t" + rightHand.transform.rotation.eulerAngles.y + "\t" + rightHand.transform.rotation.eulerAngles.z + "\t");
+			+ rightHand.transform.position.x + "\t" + rightHand.transform.position.y + "\t" + rightHand.transform.position.z + "\tx\t"
+			+ rightHand.transform.rotation.eulerAngles.x + "\t" + rightHand.transform.rotation.eulerAngles.y + "\t" + rightHand.transform.rotation.eulerAngles.z + "\t\n");
 
         file.Close();
         finished = true;
