abb2c13aa04eb4ac01fb65ef808d708e63636adb
Connie Du <c2du@ucsd.edu>
2017-07-05 14:59:00 -0700

    Added space video and calming music

---
+++ Assets/audio_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_script : MonoBehaviour
+{
+
+  static bool AudioBegin = false;
+
+  void Awake()
+  {
+    if (!AudioBegin)
+    {
+      gameObject.GetComponent<AudioSource>().Play();
+      DontDestroyOnLoad(gameObject);
+      AudioBegin = true;
+    }
+  }
+
+}
