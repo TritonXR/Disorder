80b635a16c8434b6ad7054fec515152d7d53791b
Shania Ie <sie@UCSD.EDU>
2020-02-10 16:38:33 -0800

    Cube set to appear in 5 seconds

---
+++ Assets/Scripts/TouchCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCube : MonoBehaviour {
+
+    public GameObject cube;
+    public float sec = 5f;
+
+	// Use this for initialization
+	void Start () {
+        cube.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // cube.SetActive(true);
+	}
+
+    IEnumerator disappear()
+    {
+        yield return new WaitForSeconds(sec);
+        cube.SetActive(true);
+    }
+}
